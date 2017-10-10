53f5907fdd449117d6b023126023ccdd66652f0a
SaulSandovalM <sauldevelop@gmail.com>
2017-10-10 13:11:56 -0500

    push

--- App.js
+++ App.js
@@ -5,11 +5,13 @@
 
 import React, {Component} from 'react';
 import Bienvenida from './src/components/bienvenida/Bienvenida';
+import Detalle from './src/components/detalle/Detalle';
 
 export default class App extends Component < {} > {
   render() {
     return (
     <Bienvenida/>
+    <Detalle/>
     );
   }
 }
