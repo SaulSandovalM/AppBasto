76ac26af7fa982de073b5160c9b0a22e2ac00111
JoseLAmador <josel.amador.s@gmail.com>
2017-10-10 15:21:57 -0500

    Inicio

--- App.js
+++ App.js
@@ -6,12 +6,13 @@
 import React, {Component} from 'react';
 import Bienvenida from './src/components/bienvenida/Bienvenida';
 import Detalle from './src/components/detalle/Detalle';
+import Contenido from "./src/components/Principal/Contenido";
 
 export default class App extends Component < {} > {
   render() {
     return (
-    <Bienvenida/>
-    <Detalle/>
+    <Contenido />
+
     );
   }
 }
