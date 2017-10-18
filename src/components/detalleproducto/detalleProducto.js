2c40f5b6451e91b83bfd579b70881a13cb31b55e
SaulSandovalM <sauldevelop@gmail.com>
2017-10-18 18:19:11 -0500

    Styles

--- src/components/detalleproducto/detalleProducto.js
+++ src/components/detalleproducto/detalleProducto.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, StyleSheet} from 'react-native';
 import {Header, Card} from "../comun";
 
 export default class detalleProducto extends Component < {} > {
