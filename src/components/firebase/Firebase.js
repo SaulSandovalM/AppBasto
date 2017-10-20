1ffa7d2fd380c308aa01b3fb49935d14a0a5634d
JoseLAmador <josel.amador.s@gmail.com>
2017-10-20 12:20:20 -0500

    firebase

--- src/components/firebase/Firebase.js
+++ src/components/firebase/Firebase.js
@@ -1,14 +1,13 @@
 import * as firebase from 'firebase';
 
 var config = {
-  apiKey: "AIzaSyAm3vwjUC-pSCOnwRZTirbXP-PKJ8NJ8dc",
-  authDomain: "planb-ab808.firebaseapp.com",
-  databaseURL: "https://planb-ab808.firebaseio.com",
-  projectId: "planb-ab808",
-  storageBucket: "planb-ab808.appspot.com",
-  messagingSenderId: "558603502686"
+    apiKey: "AIzaSyBrx420xJAuQM1KPmYJnAxrMIaDfcQa1vI",
+    authDomain: "eatfast-f5acf.firebaseapp.com",
+    databaseURL: "https://eatfast-f5acf.firebaseio.com",
+    projectId: "eatfast-f5acf",
+    storageBucket: "",
+    messagingSenderId: "354773888658"
 };
-
 firebase.initializeApp(config);
 export const firebaseAuth = firebase.auth()
 export default firebase;
